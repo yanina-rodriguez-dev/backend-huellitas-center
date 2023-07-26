96a36f0cb3bb566d6d31170c8da613de4f5ea075
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-07-25 22:01:58 -0300

    Agrego la configuracion de midellwares para que funcione el proyecto y  configuro el puerto

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
 
 // usar un puerto
 const app =express();
@@ -8,3 +11,11 @@ app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto '+ app.get('port'))
 })
 // middelwares: funciones que se ejecutan antes de las rutas
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+console.log(path.join(__dirname, '/public'));
+app.use(express.static(path.join(__dirname, '/public')))
+
+//https://localhost:4000
