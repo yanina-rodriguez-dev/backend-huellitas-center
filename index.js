94c94749c3ae8bad7570c33cd43244ad6e6fd5cd
Yanina Rodriguez <rodriguezyanina169@gmail.com>
2023-07-25 21:30:08 -0300

    Agrego el puerto y subo rama dev para comenzar con backend

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import express from 'express';
 
-console.log('tercerajdshkfjf')
+// usar un puerto
+const app =express();
+
+app.set('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+ app.get('port'))
+})
+// middelwares: funciones que se ejecutan antes de las rutas
