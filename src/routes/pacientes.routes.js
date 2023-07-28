d83f3a94caa5e5515080a0bdb55ba434747b745a
Ana Santillan <anisantillan10@gmail.com>
2023-07-28 04:42:45 -0300

    se agrego una nueva ruta para la peticion get que trae un producto por su id

--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { crearPaciente, obtenerPacientes } from "../controllers/pacientes.controlador";
+import { crearPaciente, obtenerPaciente, obtenerPacientes } from "../controllers/pacientes.controlador";
 
 const router = Router();
 
-router.route("/pacientes").get(obtenerPacientes).post(crearPaciente)
+router.route("/pacientes").get(obtenerPacientes).post(crearPaciente);
+router.route("/pacientes/:id").get(obtenerPaciente)
 
 export default router;
