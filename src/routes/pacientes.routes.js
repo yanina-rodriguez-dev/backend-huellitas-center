ffeb857422f9e730cafbe0a7f3afa776d84ed03a
Ana Santillan <anisantillan10@gmail.com>
2023-07-28 04:31:43 -0300

    se agrego en el archivo de rutas el post para crear paciente

--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 
-import { obtenerPacientes } from "../controllers/pacientes.controlador";
+import { crearPaciente, obtenerPacientes } from "../controllers/pacientes.controlador";
 
 const router = Router();
 
-router.route("/pacientes").get(obtenerPacientes)
+router.route("/pacientes").get(obtenerPacientes).post(crearPaciente)
 
 export default router;
