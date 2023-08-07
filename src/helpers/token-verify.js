0f11e3b86564c87e042ff5af35a7b56177a2de86
Ana Santillan <anisantillan10@gmail.com>
2023-08-07 08:35:29 -0300

    se agrego el archivo token-verify

---
+++ src/helpers/token-verify.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken'
+
+const validarJWT = (req, res, next)=>{
+    //recibir el token
+    const token = req.header('x-token');
+    if(!token){
+        //401 error en la autenticacion
+        return res.status(401).json({
+            mensaje: 'No hay token en la peticion'
+        })
+    }
+    // si el token existe
+    try{
+        const payload =  jwt.verify(token, process.env.SECRET_JWT)
+        req.nombreUsuario = payload.nombreUsuario;
+
+    }catch(error){
+        console.log(error)
+        return res.status(401).json({
+            mensaje: 'Token no valido'
+        })
+    }
+
+    next();
+}
+
+export default validarJWT;
